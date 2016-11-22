433d31fd7974a80e54020f6db3141cfb6401d362
aaronjeline <aarone@parkschool.net>
2016-11-21 22:41:54 -0500

    Reworked structure

--- Classfication.py
+++ Classfication.py
@@ -5,33 +5,41 @@ def calcEntropy(positives, negatives):
     q = positives/(positives+negatives)
     return -q * log(q,2) - (1-q) * log(1-q,2)
 
-def rateAttribute(attribute, results):
-    #Create a list of all the unique values of the attribute
-    uniques,vals = [], []
-    for i in attribute:
-        if i not in uniques:
-            uniques.append(i)
-    for i in uniques:
-        positives, negatives = 0,0
-        for j in range(len(attribute)-1):
-            if attribute[j]==i:
-                if results[j]:
+def calcEntropy(decisions, attribute):
+    uniques, results = [], []
+    for val in attribute:
+        if val not in uniques:
+            uniques.add(val)
+    for val in uniques:
+        positives, negatives = 0, 0
+        for i in range(len(attribute)-1):
+            if val == attribute[i]:
+                if decisions[i]:
                     positives += 1
                 else:
                     negatives += 1
-        vals.append((positives, negatives))
-    sums = map(lambda x: sum(x), vals)
-    entropies = map(lambda x: calcEntropy(x[0],x[1]), vals)
-    weightedAverage = None
-    return weightedAverage
-
-
-
+        sum = positives + negatives
+        entropy = calcEntropy(positives, negatives)
+        results.append((sum, entropy))
+    totalSum = sum(map(lambda x: x[1], results))
+    weightedAverage = sum(map(lambda x: x[0]*x[1], results))/totalSum
+    return (weightedAverage, uniques)
 
 
 #decisions is a list of booleans, attributes is a list of lists containing attributes
-def classify(decisions, attributes):
+def classify(decisions, attributes, name):
     #Check to make sure all the sizing works out
     assert(all(map(lambda x:len(x)==len(attributes[0]), attributes[1:])) and len(attributes[0])==len(decisions))
-    ratings= map(lambda x:rateAttribute(x, decisions), attributes)
+    entropies, uniques = zip(*list(map(lambda x: calcEntropy(decisions, x), attributes)))
+    #Decide our pivot attribute
+    pivotIndex = entropies.index(min(entropies))
+    branches = []
+    newAttributes = []
+    for i in uniques:
+
+
+
+
+
+
 
