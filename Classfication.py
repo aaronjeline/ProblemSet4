83ca6e8e5387405221bd79e39d8dc859565ef3b9
aaronjeline <aarone@parkschool.net>
2016-11-26 17:25:52 -0500

    Cosmetics

--- Classfication.py
+++ Classfication.py
@@ -1,9 +1,12 @@
 from math import log
+
+
 def calcEntropy(positives, negatives):
     if positives==0 or negatives==0:
         return 0
     q = positives/(positives+negatives)
     return -q * log(q,2) - (1-q) * log(1-q,2)
+
 
 def rankAttribute(decisions, attribute):
     uniques, results = [], []
@@ -26,16 +29,15 @@ def rankAttribute(decisions, attribute):
     return (weightedAverage, uniques)
 
 
-
-#decisions is a list of booleans, attributes is a list of lists containing attributes
+# Decisions is a list of booleans, attributes is a list of lists containing attributes
 def classify(decisions, attributes):
-    #Check to make sure all the sizing works out
+    # Check to make sure all the sizing works out
     assert(all(map(lambda x:len(x)==len(attributes[0]), attributes[1:])) and len(attributes[0])==len(decisions))
     entropies, uniques = zip(*list(map(lambda x: rankAttribute(decisions, x), attributes)))
     if all(map(lambda x:x==0,entropies)):
         #We have the simplist case, so return
         return "Done"
-    #Decide our pivot attribute
+    # Decide our pivot attribute
     pivotIndex = entropies.index(min(entropies))
     pivotAttribute = attributes[pivotIndex]
     branches = []
@@ -46,15 +48,15 @@ def classify(decisions, attributes):
         uniqueIndices = filter(lambda x: pivotAttribute[x]==i, range(len(pivotAttribute-1)))
         for j in localAttributes:
             newAttributes.append(list(map(lambda x:j[x], uniqueIndices)))
-        #Recurse, and classify the simplified list
+        # Recurse, and classify the simplified list
         branches.append(classify(decisions, newAttributes))
     return branches
 
 
-A1 = [1,1,0,1,1]
-A2 = [0,0,1,1,1]
-A3 = [0,1,0,1,0]
-Y = [False,False,False,True,True]
-LA = [A1,A2,A3]
+A1 = [1, 1, 0, 1, 1]
+A2 = [0, 0, 1, 1, 1]
+A3 = [0, 1, 0, 1, 0]
+Y = [False, False, False, True, True]
+LA = [A1, A2, A3]
 tree = classify(Y, LA)
 print(tree)
