07e2ab5f2e85cf36efd178554ed11c0ff2f325e1
Aaron Eline <aeline@parkschool.net>
2016-11-30 16:29:33 -0500

    Moved to OO design

--- Classfication.py
+++ Classfication.py
@@ -1,22 +1,58 @@
 from math import log
 
 
-def calcEntropy(positives, negatives):
-    if positives==0 or negatives==0:
-        return 0
+def calcEntropy(sortedAttribute):
+    positives = sortedAttribute.count(1)
+    negatives = len(sortedAttribute)-positives
     q = positives/(positives+negatives)
     return -q * log(q,2) - (1-q) * log(1-q,2)
 
 
 def rankAttribute(decisions, attribute):
-    positives =
+    entropy = None
+    positiveYs = []
+    negativeYs = []
+    for i in len(attribute):
+        if attribute[i] == 1:
+            positiveYs.append(decisions[i])
+        else:
+            negativeYs.append(decisions[i])
+    if len(positiveYs)==len(attribute) or len(negativeYs)==len(attribute):
+        entropy = 0
+    else:
+        ys = (positiveYs, negativeYs)
+        entropy = sum(map(lambda x: len(x) + calcEntropy(x)), ys)
+    return entropy
 
 
-class node:
+
+
+class Node:
     isLeaf = None
+    children = {}
     def node(self, trainingDecisions, trainingAttributes):
-        entropies = map(lambda x: rankAttribute(trainingAttributes, x), trainingAttributes)
+        entropies = list(map(lambda x: rankAttribute(trainingAttributes, x), trainingAttributes))
         #Check if they are all homogenous sets
+        if all(map(lambda x: x==0, entropies)):
+            #We are a leaf
+            self.isLeaf = True
+        else:
+            self.isLeaf = False
+            pivotIndex = entropies.index(max(entropies))
+            vals = (1,0)
+            for i in vals:
+                newDecision = []
+                newAttributes = []
+                for j in len(trainingAttributes)-1:
+                    newAttributes.append([])
+                #Construct the new list
+                for j in len(trainingAttributes[pivotIndex]):
+                    if trainingAttributes[pivotIndex][j]==i:
+                        for k in len(trainingAttributes):
+                            if k!=pivotIndex:
+                                newAttributes[k].append(trainingAttributes[pivotIndex][j])
+                        newDecision.append(trainingDecisions[j])
+                self.children[i] = Node(newDecision, newAttributes)
 
 
 
