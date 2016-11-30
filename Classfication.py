870dc2b3ffcb18a444ca6fee8ba99a0d5628228f
Aaron Eline <aeline@parkschool.net>
2016-11-30 14:14:24 -0500

    Work on classification

--- Classfication.py
+++ Classfication.py
@@ -9,48 +9,15 @@ def calcEntropy(positives, negatives):
 
 
 def rankAttribute(decisions, attribute):
-    uniques, results = [], []
-    for val in attribute:
-        if val not in uniques:
-            uniques.append(val)
-    for val in uniques:
-        positives, negatives = 0, 0
-        for i in range(len(attribute)):
-            if val == attribute[i]:
-                if decisions[i]:
-                    positives += 1
-                else:
-                    negatives += 1
-        localSum = positives + negatives
-        entropy = calcEntropy(positives, negatives)
-        results.append((localSum, entropy))
-    totalSum = sum(map(lambda x: x[1], results))
-    weightedAverage = sum(map(lambda x: x[0]*x[1], results))/totalSum
-    return (weightedAverage, uniques)
+    positives =
 
 
-# Decisions is a list of booleans, attributes is a list of lists containing attributes
-def classify(decisions, attributes):
-    # Check to make sure all the sizing works out
-    assert(all(map(lambda x:len(x)==len(attributes[0]), attributes[1:])) and len(attributes[0])==len(decisions))
-    entropies, uniques = zip(*list(map(lambda x: rankAttribute(decisions, x), attributes)))
-    if all(map(lambda x:x==0,entropies)):
-        #We have the simplist case, so return
-        return "Done"
-    # Decide our pivot attribute
-    pivotIndex = entropies.index(min(entropies))
-    pivotAttribute = attributes[pivotIndex]
-    branches = []
-    localAttributes = list(attributes)
-    localAttributes.pop(pivotIndex)
-    for i in uniques:
-        newAttributes = []
-        uniqueIndices = filter(lambda x: pivotAttribute[x]==i, range(len(pivotAttribute-1)))
-        for j in localAttributes:
-            newAttributes.append(list(map(lambda x:j[x], uniqueIndices)))
-        # Recurse, and classify the simplified list
-        branches.append(classify(decisions, newAttributes))
-    return branches
+class node:
+    isLeaf = None
+    def node(self, trainingDecisions, trainingAttributes):
+        entropies = map(lambda x: rankAttribute(trainingAttributes, x), trainingAttributes)
+        #Check if they are all homogenous sets
+
 
 
 A1 = [1, 1, 0, 1, 1]
